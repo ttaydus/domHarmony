5ddfa1299d159f79af0750b6f8ad58ef60a0c86f
Tyler Taydus <tylertaydus4444@gmail.com>
2018-11-23 19:21:31 -1000

    test

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
+matCount.innerHTML = 11;
+
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+
+msgCount.innerHTML = 23; asdfj
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
