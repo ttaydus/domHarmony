8937537cef5ca896d929a3ad8aa685d73fdc4214
Tyler Taydus <tylertaydus4444@gmail.com>
2018-11-23 19:25:01 -1000

    3

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@ matCount.innerHTML = 11;
 
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
 
-msgCount.innerHTML = 23; asdfj
+msgCount.innerHTML = 23; 
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
+
+fullname.innerHTML = 'Ronald McDonald';
+
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
 
